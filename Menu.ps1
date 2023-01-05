28809e2654b3dae7179b99591669cbadb0c172a8
Renato Pejon <pokeracre@gmail.com>
2023-01-05 14:44:50 -0300

    Menu changes
    Display message when select an option

--- Menu.ps1
+++ Menu.ps1
@@ -7,9 +7,11 @@ $num = Read-Host "Select a number"
 
 switch ($num) {
     1 {
+        Write-Output "Starting Genesys script..."
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Genesys.ps1'))
     }
     2 {
+        Write-Output "Starting MSERT script..."
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
     }
     
