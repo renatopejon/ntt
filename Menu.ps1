255e8ef9f356bb397fdb25ec3858065f6b908c4b
Renato Pejon <pokeracre@gmail.com>
2023-01-10 09:51:43 -0300

    Update Menu.ps1

--- Menu.ps1
+++ Menu.ps1
@@ -2,6 +2,7 @@ Write-Output "`n------------------------------------------"
 Write-Output "|         I&T Utilities Script           |"
 Write-Output "------------------------------------------`n"
 Write-Output "1 - Install Genesys with ITSM integration"
+Write-Output "2 - Do a MSERT Scan (BETA)"
 Write-Output "2 - Do a MSERT Scan`n"
 $num = Read-Host "Select a number"
 
@@ -10,9 +11,16 @@ switch ($num) {
         Write-Output "Starting Genesys script..."
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Genesys.ps1'))
     }
+
     2 {
         Write-Output "Starting MSERT script..."
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
     }
+
+    3 {
+        Write-Output "Starting MSERT script..."
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
+    }
+    
     
 }
