5096200c55f60f12006da340a34b04956a3f81de
Renato Pejon <pokeracre@gmail.com>
2023-02-16 10:49:12 -0300

    Beta version removed

--- Menu.ps1
+++ Menu.ps1
@@ -20,25 +20,17 @@ Write-Output "`------------------------------------------"
 Write-Output "|         I&T Utilities Script           |"
 Write-Output "------------------------------------------`n"
 Write-Output "1 - Install Genesys with ITSM integration"
-Write-Output "2 - Do a MSERT Scan"
-Write-Output "3 - Do a MSERT Scan (BETA)`n"
+Write-Output "2 - MSERT Scan `n"
 $num = Read-Host "Select a number"
 
 switch ($num) {
-    1 {
-        Write-Output "Starting Genesys script..."
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Genesys.ps1'))
-    }
+  1 {
+      Write-Output "Starting Genesys script..."
+      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Genesys.ps1'))
+  }
 
-    2 {
-        Write-Output "Starting MSERT script..."
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script.ps1'))
-    }
-
-    3 {
-        Write-Output "Starting MSERT script..."
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
-    }
-    
-    
+  2 {
+      Write-Output "Starting MSERT script..."
+      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
+  }      
 }
