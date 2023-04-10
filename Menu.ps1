08e3220e9c3f3286af3c49f7c3f06334b5c2bd3f
Renato Pejon <pokeracre@gmail.com>
2023-04-10 14:00:27 -0300

    Update Menu.ps1

--- Menu.ps1
+++ Menu.ps1
@@ -18,7 +18,7 @@ Write-Host "
                                                                                          
 "
 Write-Output "`------------------------------------------"
-Write-Output "|         I&T Utilities Script           |"
+Write-Output "|      LATAM I&T Utilities Script         |"
 Write-Output "------------------------------------------`n"
 Write-Output "1 - Install Genesys with ITSM integration"
 Write-Output "2 - MSERT Scan `n"
