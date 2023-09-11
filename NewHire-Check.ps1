490a8abf370451b6faf2b87b62b684e1e3953546
Renato Bessa <renato.bessa@global.ntt>
2023-09-11 11:47:03 -0300

    Update NewHire-Check.ps1
    - Added new attributes to check

--- NewHire-Check.ps1
+++ NewHire-Check.ps1
@@ -10,6 +10,13 @@ Connect-AzureAD
 Write-Host "Please, log in to the on-prem AD"
 $cred = Get-Credential
 
+$test = Test-Connection -Server 10.214.10.20
+
+if ($null -eq $test) {
+    Write-Host "Can't connect to the local resources, exiting..."
+    throw
+}
+
 function Open-File([string] $initialDirectory){
 
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
@@ -21,6 +28,8 @@ function Open-File([string] $initialDirectory){
 
     return $OpenFileDialog.filename
 } 
+
+Write-Host "Choose the CSV file to check the users"
 
 $open_file = Open-File $env:USERPROFILE
 if ($open_file -eq "") 
@@ -93,7 +102,7 @@ foreach ($user in $list.mail) {
     Write-Host -ForegroundColor Cyan "Checking on-prem AD attributes"
 
     $user = $user.Replace("@global.ntt", "")
-    $aduser = Get-ADUser -Server "na.didata.local" -Credential $cred -Identity $user -Properties * | Select-Object identitylifecyclestate, lockoutime, msExchHideFromAddressLists
+    $aduser = Get-ADUser -Server 10.214.10.20 -Credential $cred -Identity $user -Properties * | Select-Object identitylifecyclestate, lockoutime, msExchHideFromAddressLists, msNPAllowDialin, lockoutTime
 
     Write-Host -NoNewline "identitylifecyclestate "
     if ($aduser.identitylifecyclestate -eq 'active') {
@@ -102,8 +111,22 @@ foreach ($user in $list.mail) {
         Write-Host -ForegroundColor red "FAIL!"
     }
 
+    Write-Host -NoNewline "lockoutTime "
+    if ($aduser.lockoutTime -eq 0) {
+        Write-Host -ForegroundColor green "OK"
+    } else {
+        Write-Host -ForegroundColor red "FAIL!"
+    }
+
     Write-Host -NoNewline "msExchHideFromAddressLists "
     if ($aduser.msExchHideFromAddressLists -eq $false) {
+        Write-Host -ForegroundColor green "OK"
+    } else {
+        Write-Host -ForegroundColor green "FAIL"
+    }
+
+    Write-Host -NoNewline "msNPAllowDialin "
+    if ($null -eq $aduser.msNPAllowDialin) {
         Write-Host -ForegroundColor green "OK"
     } else {
         Write-Host -ForegroundColor green "FAIL"
