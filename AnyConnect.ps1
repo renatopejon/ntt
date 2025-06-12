78a6f926f41020967aab82dc590e573f6a562ee9
Renato Bessa <renato.bessa@global.ntt>
2025-06-12 14:10:31 -0300

    Added AnyConnect script and menu update

---
+++ AnyConnect.ps1
@@ -0,0 +1,68 @@
+# Config Variables
+$URL = "https://amlatstforms.blob.core.windows.net/guilhermeassuncao/AnyConnect.zip"
+$DestinationFolder ="C:\Windows\Temp"
+
+try {
+    $msiexec = Get-Process msiexec -ErrorAction SilentlyContinue
+
+    if ($null -ne $msiexec) {
+        Write-Host -NoNewline "A msiexec process was found! Trying to stop it... "
+        Stop-Process -Name "msiexec" -Force
+        Write-Host -ForegroundColor Green "OK"
+    }
+}
+catch {
+    Write-Host -ForegroundColor Red "ERROR"
+}
+
+# Download the zip file
+Try {
+
+   If(!(Test-Path -Path $DestinationFolder))
+   {
+        Write-Host -NoNewline "Creating the temp files folder... "
+        New-Item -ItemType Directory -Path $DestinationFolder | Out-Null
+        Write-host -ForegroundColor Green "OK"
+   }
+
+   Write-Host -NoNewline "Downloading files... "
+   Invoke-WebRequest -Uri $URL -OutFile 'C:\Windows\Temp\AnyConnect.zip'
+   Write-host -ForegroundColor Green "OK"
+}
+Catch {
+    Write-Host -ForegroundColor Red "ERROR"
+    Write-Host -ForegroundColor Red "Error: $($_.Exception.Message)"
+}
+
+# Extracting and installing AnyConnect
+try {
+    Expand-Archive -Force -Path "C:\Windows\Temp\AnyConnect.zip" -DestinationPath "C:\Windows\Temp\AnyConnect"
+    Set-Location "C:\Windows\Temp\AnyConnect"
+
+    Write-Host "Installing AnyConnect... " -NoNewline
+    Start-Process msiexec.exe -ArgumentList '/i "C:\Windows\Temp\AnyConnect\anyconnect-win-4.10.08029-core-vpn-predeploy-k9.msi" /quiet /norestart' -Wait
+    Write-host -ForegroundColor Green "OK"
+
+}
+catch {
+    Write-Host -ForegroundColor Red "ERROR"
+    Write-Host -ForegroundColor Red "Error: $($_.Exception.Message)"
+}
+
+# Copy profile file and remove temp files
+Write-Host "Adding MS-Service-Platform profile... " -NoNewline
+try {
+    Remove-Item "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\DDBR.xml" -Confirm:$false -Recurse -Force
+    Remove-Item "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\NTTA-Sao-Paulo.xml" -Confirm:$false -Recurse -Force
+    Remove-Item "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\NTTA-Texas.xml" -Confirm:$false -Recurse -Force
+    Copy-Item ".\*.xml" -Destination "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile"
+    Write-Host "OK" -ForegroundColor Green
+    
+    Write-Host -NoNewline "Removing temp files... "
+    Remove-Item "C:\Windows\Temp\*" -Confirm:$false -Recurse -Force
+    Write-Host "OK" -ForegroundColor Green
+}
+catch {
+    Write-Host -ForegroundColor Red "ERROR"
+    Write-Host "Error: $($_.Exception.Message)"
+}
--- Menu.ps1
+++ Menu.ps1
@@ -18,29 +18,22 @@ Write-Host "
                                                                                          
 "
 Write-Output "`------------------------------------------"
-Write-Output "|    LATAM I&T Utilities Script v4.0      |"
+Write-Output "|    LATAM I&T Utilities Script v5.0      |"
 Write-Output "------------------------------------------`n"
-Write-Output "1 - Install Genesys with ITSM integration"
-Write-Output "2 - MSERT Scan"
-Write-Output "3 - Clear data for CSIRT Tickets`n"
+Write-Output "1 - Clear data for CSIRT Tickets`n"
+Write-Output "2 - AnyConnect update for MS-Service-Platforms`n"
 $num = Read-Host "Select a number"
 
 switch ($num) {
   1 {
-      Clear-Host
-      Write-Output "Starting Genesys script..."
-      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Genesys.ps1'))
-  }
-
-  2 {
-      Clear-Host
-      Write-Output "Starting MSERT script..."
-      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/MSERT%20Script%20Public.ps1'))
-  }     
-  
-  3 {
     Clear-Host
     Write-Output "Starting Clear Files script..."
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Clear-Files.ps1'))
-} 
+  } 
+
+  2 {
+      Clear-Host
+      Write-Output "Starting AnyConnect update script..."
+      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/AnyConnect.ps1'))
+  }       
 }
