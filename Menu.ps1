06550c92baf54032794b84ec86a3f602af0cd7f1
Renato Bessa <renato.bessa@global.ntt>
2025-06-12 17:31:12 -0300

    Menu update

--- Menu.ps1
+++ Menu.ps1
@@ -27,13 +27,13 @@ $num = Read-Host "Select a number"
 switch ($num) {
   1 {
     Clear-Host
-    Write-Output "Starting Clear Files script..."
+    Write-Output "Starting Clear Files script...`n"
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/Clear-Files.ps1'))
   } 
 
   2 {
       Clear-Host
-      Write-Output "Starting AnyConnect update script..."
+      Write-Output "Starting AnyConnect update script...`n"
       Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/renatopejon/ntt/main/AnyConnect.ps1'))
   }       
 }
